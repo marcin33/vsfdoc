fa9ca59d9123c045727c0d79c2c670dcc68a25e6
Łukasz Szydło <szydler@gmail.com>
2018-10-23 09:07:09 +0200

    Initial commit

---
+++ src/main/java/com/bottega/vsfdoc/VsfdocApplication.java
@@ -0,0 +1,12 @@
+package com.bottega.vsfdoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsfdocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VsfdocApplication.class, args);
+	}
+}
